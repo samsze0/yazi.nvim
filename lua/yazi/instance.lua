a27b9f6b8bfc112817cf73e522992429ee6e8a7d
Sam Sze <mingsum.sam@gmail.com>
2024-07-13 09:36:18 +0800

    instance cleanup

--- lua/yazi/instance.lua
+++ lua/yazi/instance.lua
@@ -77,83 +77,10 @@ function PowerInstance:_setup_filepreview(opts)
   local filetypes_to_skip_preview = {}
 
   self:on_hover(function(payload)
-    -- Clear existing preview
-    self.layout.side_popup:set_lines({})
-
-    -- TODO: move logic to utils
-
-    local type = vim.fn.getftype(payload.url)
-    if type ~= "file" then
-      self:show_preview_in_nvim(false)
-      self.layout.side_popup:set_lines({ "Not a file" })
-      return
-    end
-
-    if vim.fn.filereadable(payload.url) ~= 1 then
-      self:show_preview_in_nvim(false)
-      self.layout.side_popup:set_lines({ "File not readable" })
-      return
-    end
-
-    -- local result, status, _ =
-    --   terminal_utils.system("file --mime " .. payload.url)
-    -- if status ~= 0 then
-    --   _error("Failed to get file type for " .. payload.url)
-    --   self.layout.side_popup:set_lines({ "Failed to get file type" })
-    --   return
-    -- end
-    -- ---@cast result string
-    -- if result:match("charset=binary") then
-    --   self.layout.side_popup:set_lines({ "No preview available for binary file" })
-    --   return
-    -- end
-
-    local filename = vim.fn.fnamemodify(payload.url, ":t")
-
-    local filetype = vim.filetype.match({
-      filename = filename,
+    local success = self.layout.side_popup:show_file_content(payload.url, {
+      exclude_filetypes = filetypes_to_skip_preview
     })
-    if not filetype then
-      _warn("Failed to get filetype for " .. filename)
-      self:show_preview_in_nvim(false)
-      self.layout.side_popup:set_lines({ "Failed to get filetype" })
-      return
-    end
-
-    if tbl_utils.contains(filetypes_to_skip_preview, filetype) then
-      self:show_preview_in_nvim(false)
-      self.layout.side_popup:set_lines({
-        "No preview available for filetype " .. filetype,
-      })
-      return
-    end
-
-    -- local num_of_lines = terminal_utils.system_unsafe("wc -l < " .. payload.url)
-    -- num_of_lines = tonumber(vim.trim(num_of_lines)) ---@diagnostic disable-line: cast-local-type
-    -- if not num_of_lines then
-    --   _error("Failed to get number of lines for " .. payload.url)
-    --   self.layout.side_popup:set_lines({ "Failed to get number of lines" })
-    --   return
-    -- end
-    -- if num_of_lines > 10000 then
-    --   self.layout.side_popup:set_lines({
-    --     "File is too large to preview",
-    --   })
-    --   return
-    -- end
-
-    local file_size = vim.fn.getfsize(payload.url)
-    -- Check if file_size exceeds 1MB
-    if file_size > 1024 * 1024 then
-      self:show_preview_in_nvim(false)
-      self.layout.side_popup:set_lines({
-        "File is too large to preview",
-      })
-      return
-    end
-
-    self:show_preview_in_nvim(true)
-    self.layout.side_popup:show_file_content(payload.url)
+    self:show_preview_in_nvim(success)
   end)
 end
 
